094360655dd9e2c753c5e553b3113dee22cb6d52
晨之 <chenzhi.chenzhijia@antfin.com>
2018-09-26 16:50:04 +0800

    a

--- jiekou/py1.py
+++ jiekou/py1.py
@@ -2,19 +2,10 @@ import sys
 import numpy as np
 if __name__ == "__main__":
     n = int(sys.stdin.readline().strip())
-    N = 100
-    wall = np.zeros(N)
+    N = []
     for i in range(n):
         line = sys.stdin.readline().strip()
         L = line.split()
-        l = int(L[0])
-        r = int(L[1]) + 1
-        wall[l:r] = i+1
-    wall.sort()
-    count = 0
-    for i in range(N-1):
-        if wall[i] != wall[i+1]:
-            count = count + 1  
-
+        N.append([int(c) for c in L])
     
-    print count
+    print(N)
